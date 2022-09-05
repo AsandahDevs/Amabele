ae9610335b4886c4c5de63c3d98d0f9185d9d171
Asanda <noelmajola@gmail.com>
2022-09-05 15:04:58 +0200

    Create components folder and add navigation component.

---
+++ src/Components/NavigationMenu.jsx
@@ -0,0 +1,37 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import { NavLink } from "react-router-dom";
+
+const NavMenu = () => {
+  return (
+    <Navbar bg="light" expand="md" className="mb-3">
+      <Container>
+        <Navbar.Brand as={NavLink} to="/">
+          Amabele
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls={"offcanvasNavbar-expand-md"} />
+        <Navbar.Offcanvas
+          id={"offcanvasNavbar-expand-md"}
+          aria-labelledby={"offcanvasNavbarLabel-expand-md"}
+          placement="end"
+        >
+          <Offcanvas.Header closeButton></Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="justify-content-start flex-grow-1 pe-3">
+              <Nav.Link as={NavLink} to="products">
+                Our Products
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="shoppingcart">
+                Shopping cart
+              </Nav.Link>
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavMenu;
