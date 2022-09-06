46da26e6552098e3aeecedd11fc54b6fca55fd02
Asanda <noelmajola@gmail.com>
2022-09-06 14:31:05 +0200

    Import data module. Destructure data to extract an array of products. Loop over each product information. Add cards component and assign props representing each product info. Create a div to wrap around cards component and assign class of 'horizontal-scroll'. Add input and select elements. Add fluid prop to container.

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,8 +1,44 @@
+// import { useState } from "react";
 import { Container } from "react-bootstrap";
+import Cards from "../Components/Card";
+import data from "../data";
+
 const Products = () => {
+  const { products } = data;
   return (
-    <Container>
-      <h1>Products page</h1>
+    <Container fluid>
+      <h1>Our products</h1>
+      <input
+        type="search"
+        className="form-control w-50"
+        placeholder="Enter product name"
+      />
+      <br />
+      <select>
+        <option>Sort by category</option>
+        <option>Cider</option>
+        <option>Stout</option>
+        <option>Wine</option>
+        <option>Vodka</option>
+        <option>Whiskey</option>
+        <option>Other</option>
+      </select>
+      <br />
+      <h2 style={{ marginTop: "2rem" }}>Product ranges</h2>
+      <div className="horizontal-scroller">
+        {products.map((product) => (
+          <Cards
+            key={product.id}
+            image={product.image}
+            alt={product.name}
+            price={product.price}
+            description={product.name}
+            litres={product.litres}
+            category={product.category}
+            available={product.available}
+          />
+        ))}
+      </div>
     </Container>
   );
 };
