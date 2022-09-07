afc45d9f775b2f2d6db81c4cc0f754a4237559b5
Asanda <noelmajola@gmail.com>
2022-09-07 18:03:49 +0200

    Remove select element and add conditional logic for unfound products.

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -7,12 +7,15 @@ import data from "../data";
 const Products = () => {
   const { products } = data;
   const [searchTerm, setSearchTerm] = useState("");
+
   const captureProductName = (event) => {
     setSearchTerm(event.target.value);
   };
+
   const filteredProducts = products.filter((product) => {
     return product.name.toLowerCase().includes(searchTerm.toLowerCase());
   });
+
   return (
     <Container fluid>
       <h1>Our products</h1>
@@ -24,30 +27,22 @@ const Products = () => {
         onChange={captureProductName}
       />
       <br />
-      <select>
-        <option>Sort by category</option>
-        <option>Cider</option>
-        <option>Stout</option>
-        <option>Wine</option>
-        <option>Vodka</option>
-        <option>Whiskey</option>
-        <option>Other</option>
-      </select>
-      <br />
       <h2 style={{ marginTop: "2rem" }}>Product ranges</h2>
       <div className="horizontal-scroller">
-        {filteredProducts.map((product) => (
-          <Cards
-            key={product.id}
-            image={product.image}
-            alt={product.name}
-            price={product.price}
-            description={product.name}
-            litres={product.litres}
-            category={product.category}
-            available={product.available}
-          />
-        ))}
+        {filteredProducts.length === 0
+          ? "no results found"
+          : filteredProducts.map((product) => (
+              <Cards
+                key={product.id}
+                image={product.image}
+                alt={product.name}
+                price={product.price}
+                description={product.name}
+                litres={product.litres}
+                category={product.category}
+                available={product.available}
+              />
+            ))}
       </div>
     </Container>
   );
