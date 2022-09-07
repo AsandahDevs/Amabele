65bec2eb7f7bf1ce830903acbe476045c974bb9e
Asanda <noelmajola@gmail.com>
2022-09-07 15:06:22 +0200

    Add state to capture user input value. Create function to track user input. Create a variable to store filtered products. Add onChange event handler for input element Change products to filteredProducts in jsx expression.

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,17 +1,27 @@
 // import { useState } from "react";
+import { useState } from "react";
 import { Container } from "react-bootstrap";
 import Cards from "../Components/Card";
 import data from "../data";
 
 const Products = () => {
   const { products } = data;
+  const [searchTerm, setSearchTerm] = useState("");
+  const captureProductName = (event) => {
+    setSearchTerm(event.target.value);
+  };
+  const filteredProducts = products.filter((product) => {
+    return product.name.toLowerCase().includes(searchTerm.toLowerCase());
+  });
   return (
     <Container fluid>
       <h1>Our products</h1>
       <input
         type="search"
+        value={searchTerm}
         className="form-control w-50"
         placeholder="Enter product name"
+        onChange={captureProductName}
       />
       <br />
       <select>
@@ -26,7 +36,7 @@ const Products = () => {
       <br />
       <h2 style={{ marginTop: "2rem" }}>Product ranges</h2>
       <div className="horizontal-scroller">
-        {products.map((product) => (
+        {filteredProducts.map((product) => (
           <Cards
             key={product.id}
             image={product.image}
