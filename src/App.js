8a51cfe70c13683a63f0b65acf7490c0b3ac89fa
Asanda <noelmajola@gmail.com>
2022-09-09 16:18:05 +0200

    Import cartprovider component from 'react-use-cart' library.

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import SharedLayout from "./pages/SharedLayout";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import ShoppingCart from "./pages/ShoppingCart";
+import { CartProvider } from "react-use-cart";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<Home />} />
-          <Route path="products" element={<Products />} />
-          <Route path="shoppingcart" element={<ShoppingCart />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <CartProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SharedLayout />}>
+            <Route index element={<Home />} />
+            <Route path="products" element={<Products />} />
+            <Route path="shoppingcart" element={<ShoppingCart />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </CartProvider>
   );
 }
 
