a48c58609aca815731b533daf163d9cddf897021
Asanda <noelmajola@gmail.com>
2022-09-09 16:42:25 +0200

    Import useCart component. Destructure variables and methods from the useCart hook. Apply conditional logic for displaying products when cart is either full or empty.

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,8 +1,44 @@
 import { Container } from "react-bootstrap";
+import { useCart } from "react-use-cart";
 const ShoppingCart = () => {
+  const {
+    isEmpty,
+    totalUniqueItems,
+    items,
+    totalItems,
+    cartTotal,
+    updateItemQuantity,
+    removeItem,
+    emptyCart,
+  } = useCart();
   return (
     <Container>
       <h1>Shopping cart page</h1>
+      {isEmpty ? (
+        <h2 style={{ color: "red" }}>Cart is empty</h2>
+      ) : (
+        <table className="table table-dark table-hover m-0">
+          <tbody>
+            {items.map((item) => {
+              return (
+                <tr key={item.id}>
+                  <td>
+                    <img
+                      src={item.image}
+                      style={{ height: "6rem" }}
+                      alt={item.name}
+                    />
+                  </td>
+                  <td>{item.name}</td>
+                  <td>{item.description}</td>
+                  <td>Unit price: R{item.price}</td>
+                  <td>Quantity: {item.quantity}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
     </Container>
   );
 };
