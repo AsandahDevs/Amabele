f8d3594ff4eb8fc89134e1510e5c6fdd4c84c5df
Asanda <noelmajola@gmail.com>
2022-09-09 18:03:03 +0200

    Add responsiveness for table element.

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -14,64 +14,66 @@ const ShoppingCart = () => {
     emptyCart,
   } = useCart();
   return (
-    <Container>
+    <Container fluid>
       <h1>Shopping cart page</h1>
       {isEmpty ? (
         <h2 style={{ color: "red" }}>Cart is empty</h2>
       ) : (
         <>
-          <table className="table table-dark table-hover m-0">
-            <tbody>
-              {items.map((item) => {
-                return (
-                  <tr key={item.id}>
-                    <td>
-                      <img
-                        src={item.image}
-                        style={{ height: "6rem" }}
-                        alt={item.name}
-                      />
-                    </td>
-                    <td>{item.name}</td>
-                    <td>{item.description}</td>
-                    <td>Unit price: R{item.price}</td>
-                    <td>
-                      <Button
-                        onClick={() =>
-                          updateItemQuantity(item.id, item.quantity + 1)
-                        }
-                      >
-                        +
-                      </Button>{" "}
-                      Quantity: {item.quantity}{" "}
-                      <Button
-                        variant="outline-warning"
-                        onClick={() =>
-                          updateItemQuantity(item.id, item.quantity - 1)
-                        }
-                      >
-                        -
-                      </Button>
-                    </td>
-                    <td>
-                      <Badge
-                        bg="danger"
-                        onClick={() => removeItem(item.id)}
-                        style={{ cursor: "pointer" }}
-                      >
-                        x
-                      </Badge>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
+          <div className="table-responsive">
+            <table className="table table-dark table-hover table-striped ">
+              <tbody>
+                {items.map((item) => {
+                  return (
+                    <tr key={item.id}>
+                      <td>
+                        <img
+                          src={item.image}
+                          style={{ height: "6rem" }}
+                          alt={item.name}
+                        />
+                      </td>
+                      <td>{item.name}</td>
+                      <td>{item.description}</td>
+                      <td>Unit price: R{item.price}</td>
+                      <td>
+                        <Button
+                          onClick={() =>
+                            updateItemQuantity(item.id, item.quantity + 1)
+                          }
+                        >
+                          +
+                        </Button>{" "}
+                        Quantity: {item.quantity}{" "}
+                        <Button
+                          variant="outline-warning"
+                          onClick={() =>
+                            updateItemQuantity(item.id, item.quantity - 1)
+                          }
+                        >
+                          -
+                        </Button>
+                      </td>
+                      <td>
+                        <Badge
+                          bg="danger"
+                          onClick={() => removeItem(item.id)}
+                          style={{ cursor: "pointer" }}
+                        >
+                          x
+                        </Badge>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
           <h3
             style={{
               color: "GrayText",
               fontSize: "1.5rem",
-              marginLeft: "55rem",
+              marginLeft: "1rem",
             }}
           >
             Cart total:R {cartTotal}
