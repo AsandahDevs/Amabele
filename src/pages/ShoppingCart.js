5ab6162c11f03905911364e5bd3f72196b43f04a
Asanda <noelmajola@gmail.com>
2022-09-09 17:36:39 +0200

    Create h3 element and add variable to display total cart amount.

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -19,53 +19,64 @@ const ShoppingCart = () => {
       {isEmpty ? (
         <h2 style={{ color: "red" }}>Cart is empty</h2>
       ) : (
-        <table className="table table-dark table-hover m-0">
-          <tbody>
-            {items.map((item) => {
-              return (
-                <tr key={item.id}>
-                  <td>
-                    <img
-                      src={item.image}
-                      style={{ height: "6rem" }}
-                      alt={item.name}
-                    />
-                  </td>
-                  <td>{item.name}</td>
-                  <td>{item.description}</td>
-                  <td>Unit price: R{item.price}</td>
-                  <td>
-                    <Button
-                      onClick={() =>
-                        updateItemQuantity(item.id, item.quantity + 1)
-                      }
-                    >
-                      +
-                    </Button>{" "}
-                    Quantity: {item.quantity}{" "}
-                    <Button
-                      variant="outline-warning"
-                      onClick={() =>
-                        updateItemQuantity(item.id, item.quantity - 1)
-                      }
-                    >
-                      -
-                    </Button>
-                  </td>
-                  <td>
-                    <Badge
-                      bg="danger"
-                      onClick={() => removeItem(item.id)}
-                      style={{ cursor: "pointer" }}
-                    >
-                      x
-                    </Badge>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <>
+          <table className="table table-dark table-hover m-0">
+            <tbody>
+              {items.map((item) => {
+                return (
+                  <tr key={item.id}>
+                    <td>
+                      <img
+                        src={item.image}
+                        style={{ height: "6rem" }}
+                        alt={item.name}
+                      />
+                    </td>
+                    <td>{item.name}</td>
+                    <td>{item.description}</td>
+                    <td>Unit price: R{item.price}</td>
+                    <td>
+                      <Button
+                        onClick={() =>
+                          updateItemQuantity(item.id, item.quantity + 1)
+                        }
+                      >
+                        +
+                      </Button>{" "}
+                      Quantity: {item.quantity}{" "}
+                      <Button
+                        variant="outline-warning"
+                        onClick={() =>
+                          updateItemQuantity(item.id, item.quantity - 1)
+                        }
+                      >
+                        -
+                      </Button>
+                    </td>
+                    <td>
+                      <Badge
+                        bg="danger"
+                        onClick={() => removeItem(item.id)}
+                        style={{ cursor: "pointer" }}
+                      >
+                        x
+                      </Badge>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          <h3
+            style={{
+              color: "GrayText",
+              fontSize: "1.5rem",
+              marginLeft: "55rem",
+            }}
+          >
+            Cart total:R {cartTotal}
+          </h3>
+        </>
       )}
     </Container>
   );
