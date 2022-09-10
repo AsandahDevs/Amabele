39f1d8c36169c7faed9294ad93efc5c4a588df1f
Asanda <noelmajola@gmail.com>
2022-09-11 00:28:30 +0200

    Add footer component.

--- src/pages/SharedLayout.js
+++ src/pages/SharedLayout.js
@@ -1,10 +1,12 @@
 import NavMenu from "../Components/NavigationMenu";
 import { Outlet } from "react-router-dom";
+import Footer from "../Components/Footer";
 const SharedLayout = () => {
   return (
     <>
       <NavMenu />
       <Outlet />
+      <Footer />
     </>
   );
 };
