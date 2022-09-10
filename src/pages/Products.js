0e17b73e230cc76b62abb0f434911bccd0ad92e3
Asanda <noelmajola@gmail.com>
2022-09-11 00:13:15 +0200

    Revert "Add useEffect hook for accessing DOM nodes."
    This reverts commit a03dfc87f6fdcd11b1dc187ea41a41e381cc33d6.

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,7 +1,6 @@
 // import { useState } from "react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Container } from "react-bootstrap";
-import { ChevronRight, ChevronLeft } from "react-bootstrap-icons";
 import Cards from "../Components/Card";
 import data from "../data";
 
@@ -32,27 +31,6 @@ const Products = () => {
     day += "Looks like someone is working night shifts.";
   }
 
-  //  A function that will handle the functionality of the directional arrows.
-  useEffect(() => {
-    const containedElements = [
-      ...document.querySelectorAll(".horizontal-scroller.container"),
-    ];
-    const nextBtn = [...document.querySelectorAll(".next-btn")];
-    const prevBtn = [...document.querySelectorAll(".prev-btn")];
-
-    containedElements.forEach((element, index) => {
-      let containerDimensions = element.getBoundingClientRect(); // retrieves the dimensions of the contained elements.
-      let containerWidth = containerDimensions.width;
-
-      nextBtn[index].addEventListener("click", () => {
-        element.scrollLeft += containerWidth;
-      });
-      // This makes an arrow button scroll in a negative direction, therefore creating a "left" scroll effect.
-      prevBtn[index].addEventListener("click", () => {
-        element.scrollLeft -= containerWidth;
-      });
-    });
-  }, []);
   return (
     <Container fluid>
       <h1>Our products</h1>
@@ -66,12 +44,7 @@ const Products = () => {
       />
       <br />
       <h2 style={{ marginTop: "2rem" }}>Product ranges</h2>
-      <div className="horizontal-scroller container">
-        <span className="prev-btn">
-          <ChevronLeft
-            style={{ color: "hotpink", marginRight: "2rem", fontSize: "2rem" }}
-          />
-        </span>
+      <div className="horizontal-scroller">
         {filteredProducts.length === 0
           ? "no results found"
           : filteredProducts.map((item) => (
@@ -87,11 +60,6 @@ const Products = () => {
                 item={item}
               />
             ))}
-        <span className="next-btn">
-          <ChevronRight
-            style={{ color: "hotpink", marginLeft: "2rem", fontSize: "2rem" }}
-          />
-        </span>
       </div>
     </Container>
   );
