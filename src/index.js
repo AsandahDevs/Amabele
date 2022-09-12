de71f28060e43ee0599d3268d1e375fbd5c99369
Asanda <noelmajola@gmail.com>
2022-09-12 16:12:46 +0200

    Import swiper css bundle.
    This bundle contains all swiper css styles for each module.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
+import "swiper/css/bundle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
