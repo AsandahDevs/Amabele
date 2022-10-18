3d3fe191f86a282c4067b54b2f44e886d886ab16
Asanda <noelmajola@gmail.com>
2022-10-18 15:08:04 +0200

    Add global state and assign props to home and product components.
    I've moved state up by one level from the home component, so that all the form data can be exposed to the entire application in order to dynamically render usernames next to the greeting messages in the products component/page.

--- src/App.js
+++ src/App.js
@@ -4,15 +4,55 @@ import Home from "./pages/Home";
 import Products from "./pages/Products";
 import ShoppingCart from "./pages/ShoppingCart";
 import { CartProvider } from "react-use-cart";
+import { useState } from "react";
 
 function App() {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    confirmPassword: "",
+    showPassword: false,
+    showConfirmPassword: false,
+  });
+
+  const handleChange = (event) => {
+    setFormData((initialFormData) => {
+      return { ...initialFormData, [event.target.name]: event.target.value };
+    });
+  };
+
+  const handlePasswordChange = () => {
+    setFormData((initialFormData) => {
+      return { ...initialFormData, showPassword: !formData.showPassword };
+    });
+  };
+
+  const handlePasswordChange2 = () => {
+    setFormData((initialFormData) => {
+      return {
+        ...initialFormData,
+        showConfirmPassword: !formData.showConfirmPassword,
+      };
+    });
+  };
+
   return (
     <CartProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/Amabele" element={<SharedLayout />}>
-            <Route index element={<Home />} />
-            <Route path="products" element={<Products />} />
+            <Route
+              index
+              element={
+                <Home
+                  formData={formData}
+                  handleChange={handleChange}
+                  handlePasswordChange={handlePasswordChange}
+                  handleConfirmPassword={handlePasswordChange2}
+                />
+              }
+            />
+            <Route path="products" element={<Products formData={formData} />} />
             <Route path="shoppingcart" element={<ShoppingCart />} />
           </Route>
         </Routes>
