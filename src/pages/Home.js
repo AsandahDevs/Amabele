fd2d4f30254af18f626e2875a99662f4e1f600e1
Asanda <noelmajola@gmail.com>
2022-10-18 15:14:03 +0200

    Move up state by one level to the main/application component.Add props for consuming incoming data.Add value attribute for submit button.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,48 +1,18 @@
-import { useState } from "react";
 import { Container } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
 import { EyeSlashFill, EyeFill } from "react-bootstrap-icons";
+import { useNavigate } from "react-router-dom";
 
-const Home = () => {
-  const [formData, setFormData] = useState({
-    username: "",
-    password: "",
-    confirmPassword: "",
-    showPassword: false,
-    showConfirmPassword: false,
-  });
-
-  const handleChange = (event) => {
-    setFormData((initialFormData) => {
-      return { ...initialFormData, [event.target.name]: event.target.value };
-    });
-  };
-
-  const handlePasswordChange = () => {
-    setFormData((initialFormData) => {
-      return { ...initialFormData, showPassword: !formData.showPassword };
-    });
-  };
-
-  const handlePasswordChange2 = () => {
-    setFormData((initialFormData) => {
-      return {
-        ...initialFormData,
-        showConfirmPassword: !formData.showConfirmPassword,
-      };
-    });
-  };
+const Home = (props) => {
   const navigate = useNavigate();
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (formData.password === formData.confirmPassword) {
+    if (props.formData.password === props.formData.confirmPassword) {
       navigate("/Amabele/products");
     } else {
       alert("Passwords do not match");
     }
   };
-
   return (
     <Container fluid>
       <h1 style={{ paddingTop: "1rem" }}>Login page</h1>
@@ -54,8 +24,8 @@ const Home = () => {
             name="username"
             className="form-control"
             placeholder="Enter your username"
-            onChange={handleChange}
-            value={formData.username}
+            onChange={props.handleChange}
+            value={props.formData.username}
             required
           />
         </label>
@@ -64,16 +34,19 @@ const Home = () => {
           Password
           <div className="input-group">
             <input
-              type={formData.showPassword ? "text" : "password"}
+              type={props.formData.showPassword ? "text" : "password"}
               name="password"
               className="form-control"
               placeholder="***"
-              onChange={handleChange}
-              value={formData.password}
+              onChange={props.handleChange}
+              value={props.formData.password}
               required
             />
-            <span className="input-group-text" onClick={handlePasswordChange}>
-              {formData.showPassword ? <EyeFill /> : <EyeSlashFill />}
+            <span
+              className="input-group-text"
+              onClick={props.handlePasswordChange}
+            >
+              {props.formData.showPassword ? <EyeFill /> : <EyeSlashFill />}
             </span>
           </div>
         </label>
@@ -82,22 +55,29 @@ const Home = () => {
           Confirm password
           <div className="input-group">
             <input
-              type={formData.showConfirmPassword ? "text" : "password"}
+              type={props.formData.showConfirmPassword ? "text" : "password"}
               name="confirmPassword"
               className="form-control"
               placeholder="***"
-              onChange={handleChange}
-              value={formData.confirmPassword}
+              onChange={props.handleChange}
+              value={props.formData.confirmPassword}
               required
             />
-            <span className="input-group-text" onClick={handlePasswordChange2}>
-              {formData.showConfirmPassword ? <EyeFill /> : <EyeSlashFill />}
+            <span
+              className="input-group-text"
+              onClick={props.handleConfirmPassword}
+            >
+              {props.formData.showConfirmPassword ? (
+                <EyeFill />
+              ) : (
+                <EyeSlashFill />
+              )}
             </span>
           </div>
         </label>
         <br />
         <label style={{ marginTop: "2rem" }}>
-          <input type="submit" className="btn btn-success" />
+          <input type="submit" className="btn btn-success" value="login" />
         </label>
       </form>
     </Container>
